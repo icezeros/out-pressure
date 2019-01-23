5573a194b7d581516c46f9726d51fe754a00f092
icezeros <icezeros@163.com>
2019-01-23 18:53:55 +0800

    init

---
+++ view/src/defaultSettings.js
@@ -0,0 +1,28 @@
+// module.exports = {
+//   navTheme: 'dark', // theme for nav menu
+//   primaryColor: '#1890FF', // primary color of ant design
+//   layout: 'sidemenu', // nav menu position: sidemenu or topmenu
+//   contentWidth: 'Fluid', // layout of content: Fluid or Fixed, only works when layout is topmenu
+//   fixedHeader: false, // sticky header
+//   autoHideHeader: false, // auto hide header
+//   fixSiderbar: false, // sticky siderbar
+//   menu: {
+//     disableLocal: false,
+//   },
+//   title: 'Ant Design Pro',
+// };
+
+module.exports = {
+  navTheme: 'dark',
+  primaryColor: '#1890FF',
+  layout: 'sidemenu',
+  contentWidth: 'Fluid',
+  fixedHeader: true,
+  autoHideHeader: false,
+  fixSiderbar: true,
+  menu: {
+    disableLocal: false,
+  },
+  title: 'Ant Design Pro',
+  collapse: true,
+};
