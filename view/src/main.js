8607b444299d9b5b55668b226ea8933f70006d83
icezeros <icezeros@163.com>
2019-06-06 15:43:14 +0800

    save

--- view/src/main.js
+++ view/src/main.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-sequences */
 // ie polyfill
 import '@babel/polyfill'
 
@@ -15,6 +16,19 @@ import './core/use'
 // import './permission' // permission control
 import './utils/filter' // global filter
 
+function ipcMessage () {
+  ipcRenderer.send('asynchronous-message', 'ping')
+  ipcRenderer.on('pressure', (event, arg) => {
+    console.log('============  =============')
+    console.log()
+
+    console.log(arg) // prints "pong"
+    // event.sender.send('asynchronous-message', 'ping')
+
+    store.dispatch('SetPressure', arg)
+  })
+}
+
 Vue.config.productionTip = false
 
 // mount axios Vue.$http and this.$http
@@ -23,8 +37,9 @@ Vue.use(VueAxios)
 new Vue({
   router,
   store,
-  created() {
-    bootstrap()
+  created () {
+    // eslint-disable-next-line no-unused-expressions
+    bootstrap(), ipcMessage()
   },
   render: h => h(App)
 }).$mount('#app')
